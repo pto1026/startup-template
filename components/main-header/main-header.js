0880ae54a810db03e00cb78455cfa90f7081a628
Patrick Sparks <pto1026@gmail.com>
2022-03-12 19:48:09 -0700

    initial

---
+++ components/main-header/main-header.js
@@ -0,0 +1,13 @@
+export default function MainHeader() {
+  return (
+    <div className="main-header">
+      <h2 className="heading-secondary">Transforming business online</h2>
+      <h1 className="heading-primary">
+        Analytics that transcend your project&apos;s reality.
+      </h1>
+      <a href="#" className="button-primary">
+        Buy now
+      </a>
+    </div>
+  );
+}
